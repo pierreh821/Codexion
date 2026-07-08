573f27bf7debd5e8e51dc78247d7ca3f51bd57ad
Pierre Henry <phenry@k2r3p6.42mulhouse.fr>
2026-07-08 18:23:50 +0200

    add: time for monitor

--- models/monitor.h
+++ models/monitor.h
@@ -6,17 +6,20 @@
 /*   By: phenry <phenry@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/06 17:51:59 by phenry            #+#    #+#             */
-/*   Updated: 2026/07/06 17:53:14 by phenry           ###   ########.fr       */
+/*   Updated: 2026/07/08 18:23:35 by phenry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MONITOR_H
 # define MONITOR_H
 # include <pthread.h>
+# include <time.h>
 
 typedef struct s_monitor
 {
-	pthread_t	thread_id;
+	pthread_t		thread_id;
+	struct timeval	start_tm;
+	struct timeval	tm;
 }	t_monitor;
 
 #endif
