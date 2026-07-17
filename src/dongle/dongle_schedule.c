76f2ceec5076651915e7d40d86bd508cab918177
Pierre <pierrehenry.cql@gmail.com>
2026-07-18 00:32:21 +0200

    add: edf_cmp

--- src/dongle/dongle_schedule.c
+++ src/dongle/dongle_schedule.c
@@ -6,7 +6,7 @@
 /*   By: phenry <phenry@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/16 23:07:23 by phenry            #+#    #+#             */
-/*   Updated: 2026/07/18 00:05:27 by phenry           ###   ########.fr       */
+/*   Updated: 2026/07/18 00:32:07 by phenry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,14 @@ int	check_running_dongle(t_dongle *dongle)
 
 int	edf_cmp(t_waiter *a, t_waiter *b)
 {
-	return (a->priority < b->priority); // for now, change later
+	long	deadline_a;
+	long	deadline_b;
+
+	deadline_a = a->coder->start + a->coder->table->args->time_to_burnout;
+	deadline_b = b->coder->start + b->coder->table->args->time_to_burnout;
+	if (deadline_a != deadline_b)
+		return (deadline_a < deadline_b);
+	return (a->coder->id < b->coder->id);
 }
 
 int	fifo_cmp(t_waiter *a, t_waiter *b)
