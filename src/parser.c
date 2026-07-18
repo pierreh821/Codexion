7abd509737d399ca6b8a17e28469b15de52d954d
Pierre <pierrehenry.cql@gmail.com>
2026-07-18 16:55:01 +0200

    add: max nb coders fix: parser error on stderr

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: phenry <phenry@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/18 02:22:32 by phenry            #+#    #+#             */
-/*   Updated: 2026/07/18 11:40:51 by phenry           ###   ########.fr       */
+/*   Updated: 2026/07/18 16:52:49 by phenry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,32 +15,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
-int	args_validator(int argc, char *argv[])
+int	input_validator(int argc, char *argv[])
 {
 	if (argc != 9)
 		return ((void)fprintf(stderr, "Invalid number of arguments\n"), 0);
 	if (!ft_isnumber(argv[1]))
-		return ((void)printf("number_of_coders must be a non-negative \
-integer\n"), 0);
+		return ((void)fprintf(stderr, "number_of_coders must be a \
+non-negative integer\n"), 0);
 	if (!ft_isnumber(argv[2]))
-		return ((void)printf("time_to_burnout must be a non-negative \
-integer\n"), 0);
+		return ((void)fprintf(stderr, "time_to_burnout must be a \
+non-negative integer\n"), 0);
 	if (!ft_isnumber(argv[3]))
-		return ((void)printf("time_to_compile must be a non-negative \
-integer\n"), 0);
+		return ((void)fprintf(stderr, "time_to_compile must be a \
+non-negative integer\n"), 0);
 	if (!ft_isnumber(argv[4]))
-		return ((void)printf("time_to_debug must be a non-negative \
-integer\n"), 0);
+		return ((void)fprintf(stderr, "time_to_debug must be a \
+non-negative integer\n"), 0);
 	if (!ft_isnumber(argv[5]))
-		return ((void)printf("time_to_refactor must be a non-negative \
-integer\n"), 0);
+		return ((void)fprintf(stderr, "time_to_refactor must be a \
+non-negative integer\n"), 0);
 	if (!ft_isnumber(argv[6]))
-		return ((void)printf("number_of_compiles must be a non-negative \
-integer\n"), 0);
+		return ((void)fprintf(stderr, "number_of_compiles must be a \
+non-negative integer\n"), 0);
 	if (!ft_isnumber(argv[7]))
-		return ((void)printf("dongle_cooldown must be a non-negative \
-integer\n"), 0);
+		return ((void)fprintf(stderr, "dongle_cooldown must be a \
+non-negative integer\n"), 0);
+	return (1);
+}
+
+
+int	args_validator(t_args *args)
+{
+	if (args->number_of_coders > 300)
+	{
+		fprintf(stderr, "number_of_coder must be less or equal to 300\n");
+		return (0);
+	}
 	return (1);
 }
 
@@ -48,7 +60,7 @@ t_args	*clean_args(int argc, char *argv[])
 {
 	t_args	*args;
 
-	if (!args_validator(argc, argv))
+	if (!input_validator(argc, argv))
 		return (NULL);
 	args = ft_calloc(1, sizeof(t_args));
 	args->number_of_coders = atoi(argv[1]);
@@ -59,11 +71,9 @@ t_args	*clean_args(int argc, char *argv[])
 	args->number_of_compiles_required = atoi(argv[6]);
 	args->dongle_cooldown = atoi(argv[7]);
 	if (!set_schedule(args, argv[8]))
-	{
-		printf("scheduler must be fifo or edf\n");
-		free(args);
-		return (NULL);
-	}
+		return (free(args), NULL);
+	if (!args_validator(args))
+		return (free(args), NULL);
 	return (args);
 }
 
@@ -74,6 +84,9 @@ int	set_schedule(t_args *args, char *input)
 	else if (strcmp(input, "edf") == 0)
 		args->strategy = EDF;
 	else
+	{
+		printf("scheduler must be fifo or edf\n");
 		return (0);
+	}
 	return (1);
 }
